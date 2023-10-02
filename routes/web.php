60f0a6ec82a0ff76a307f028f35f82347b009394
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-10-02 10:38:52 +0700

    add AdminAuthMiddleware feature

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,14 @@ Route::get("/products", 'App\Http\Controllers\ProductController@index')->name("p
 Route::get("/products/{id}", 'App\Http\Controllers\ProductController@show')->name("product.show");
 
 // Admin pages
-Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
-Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name('admin.product.index');
-Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name('admin.product.store');
-Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
-Route::get('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
-Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
+// Middleware Applying to Admin Role
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
+    Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name('admin.product.index');
+    Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name('admin.product.store');
+    Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
+    Route::get('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
+    Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
+});
 
 Auth::routes();
