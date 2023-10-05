48ed4d217d122171aedc05522e9f0b7d85a0d780
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-10-05 10:44:38 +0700

    change composer.lock

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,15 @@ Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('
 
 // Admin pages
 // Middleware Applying to Admin Role
-Route::middleware('admin')->group(function () {
-    Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
-    Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name('admin.product.index');
-    Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name('admin.product.store');
-    Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
-    Route::get('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
-    Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
-});
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
+Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name('admin.product.index');
+Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name('admin.product.store');
+Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
+Route::get('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
+Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
+
+//Route::middleware('admin')->group(function () {
+//
+//});
 
 Auth::routes();
